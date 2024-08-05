c72f42539510a0b5055d8bacc62cdc0a0c60b709
Josué Ávalos <aezequiel56@gmail.com>
2024-08-05 17:49:04 -0600

    Classes, constructors, this operator and overloading

--- src/poo/concepts/models/Person.java
+++ src/poo/concepts/models/Person.java
@@ -1,9 +1,18 @@
-package models;
+package poo.concepts.models;
 
 public class Person {
     private String name;
     private String lastname;
     private String email;
+
+    public Person() {
+    }
+
+    public Person(String name, String lastname, String email) {
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+    }
 
     public void showPerson() {
         System.out.println("The name is " + name + ", the lastname " + lastname
